a236bb497e6a56c6b1ef11f2335e7a5c0948d63d
Farheen Malik <farheen.m.malik@gmail.com>
2015-02-01 21:09:50 -0500

    Seeding database
    Add methods for seeding schools db using the csv.  Add method to create full street address which is geocoded and lat and long is added with help of Bing/geocoder.  Add method to add opening date field information using Chronic gem.  Add method to seed building table with information from schools table.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,48 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+require 'csv'
+
+School.delete_all 
+Building.delete_all
+
+csv_text = File.read('db/colocated-charters-and-schools.csv')
+csv = CSV.parse(csv_text, :headers => true)
+
+csv.each do |row|
+  School.create!(row.to_hash)
+end
+
+all_schools = School.all
+
+#first iteration over all schools to add full_street_address
+all_schools.each do |school|
+	full_address = "#{school.primary_address}, #{school.city}, #{school.state_code} #{school.zip}"
+	school.update(full_street_address:full_address)
+	school.save
+end
+
+# fix geocoding problem
+while School.where(latitude: nil).count > 0
+	schools_without_latlong = School.where(latitude: nil)
+	schools_without_latlong.each do |school|
+		full_address = "#{school.primary_address}, #{school.city}, #{school.state_code} #{school.zip}"
+		school.update(full_street_address: full_address)
+		school.save
+	end
+end
+
+# add formatted opening date to schools
+all_schools.each do |school|
+	date = Chronic.parse(school.open_date)
+	school.update(opening_date: date.to_date)
+	school.save
+end
+
+# add building information
+all_schools.each do |school|
+	building = Building.find_or_create_by(primary_address: school.primary_address)
+	building.schools << school
+end
